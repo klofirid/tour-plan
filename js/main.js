e1f2c1d0508c11e1655d6220ad9436f7803747cb
klofirid <olvamipy@mail.ru>
2026-07-23 17:55:02 +0300

    create: rewiews slider, fix media

--- js/main.js
+++ js/main.js
@@ -1,25 +1,53 @@
-const swiper = new Swiper('.swiper', {
+const hotelSwiper = new Swiper(".hotel-slider", {
   loop: true,
   slidesPerView: 1,
   spaceBetween: 0,
   speed: 300,
-  
+
   navigation: {
-    nextEl: '.slider-button-next',
-    prevEl: '.slider-button-prev',
+    nextEl: ".hotel-slider__button--next",
+    prevEl: ".hotel-slider__button--prev",
   },
-  
+
+  pagination: {
+    el: ".hotel-slider .swiper-pagination",
+    clickable: true,
+  },
+
   // Управление с клавиатуры
   keyboard: {
     enabled: true,
     onlyInViewport: true,
     pageUpDown: true,
   },
-  
+
   // Автообновление
   watchSlidesProgress: true,
   watchSlidesVisibility: true,
-  
+
+  observer: true,
+  observeParents: true,
+});
+
+const reviewsSwiper = new Swiper(".reviews-slider", {
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 0,
+  speed: 300,
+  watchOverflow: false,
+
+  navigation: {
+    nextEl: ".reviews-slider__button--next",
+    prevEl: ".reviews-slider__button--prev",
+  },
+
+  // Управление с клавиатуры
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+    pageUpDown: true,
+  },
+
   observer: true,
   observeParents: true,
 });
